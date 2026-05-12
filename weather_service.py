6f08cee3409deaa64501fcc078dcab794ab43648
dreamtlee <deniil-bog@mail.ru>
2026-05-12 20:58:57 +0700

    Update weather_service.py

--- weather_service.py
+++ weather_service.py
@@ -1,32 +1,33 @@
 import requests
 from config import Config
 
-def get_weather(city="Москва"):
+def get_weather(city):
     try:
-        geo_url = "https://geocode-maps.yandex.ru/1.x/"
-        geo_params = {
+        geocode_url = "http://geocode-maps.yandex.ru/1.x/"
+        geocode_params = {
             "apikey": Config.YANDEX_GEO_API_KEY,
             "geocode": city,
             "format": "json"
         }
-        geo_data = requests.get(geo_url, params=geo_params).json()
-        pos = geo_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-        lon, lat = pos.split()
+        geo_data = requests.get(geocode_url, params=geocode_params).json()
+        coordinates = geo_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+        lon, lat = coordinates.split()
 
         weather_url = "https://api.weather.yandex.ru/v2/informers"
-        headers = {'X-Yandex-API-Key': Config.YANDEX_WEATHER_API_KEY}
-        w_params = {'lat': lat, 'lon': lon, 'lang': 'ru_RU'}
+        weather_headers = {"X-Yandex-API-Key": Config.YANDEX_WEATHER_API_KEY}
+        weather_params = {"lat": lat, "lon": lon, "lang": "ru_RU"}
         
-        res = requests.get(weather_url, params=w_params, headers=headers).json()
-        temp = res['fact']['temp']
+        weather_data = requests.get(weather_url, params=weather_params, headers=weather_headers).json()
+        temperature = weather_data['fact']['temp']
+        condition = weather_data['fact']['condition']
         
-        conditions = {
+        conditions_map = {
             'clear': 'ясно', 'partly-cloudy': 'малооблачно', 'cloudy': 'облачно',
             'overcast': 'пасмурно', 'drizzle': 'морось', 'light-rain': 'небольшой дождь',
-            'rain': 'дождь', 'heavy-rain': 'сильный дождь', 'snow': 'снег'
+            'rain': 'дождь', 'moderate-rain': 'умеренно сильный дождь', 'heavy-rain': 'сильный дождь'
         }
-        cond = conditions.get(res['fact']['condition'], res['fact']['condition'])
-
-        return f"В городе {city} сейчас {temp}°C, {cond}. Отличное время для тренировки!"
-    except Exception:
-        return "Не удалось получить данные о погоде. Но пробежка всё равно пойдет на пользу!"
+        readable_cond = conditions_map.get(condition, condition)
+        
+        return f"В городе {city} сейчас {temperature}°C, {readable_cond}. Прекрасные условия для достижения новых рекордов!"
+    except Exception as e:
+        return "Сервис погоды временно недоступен, но это не повод пропускать тренировку!"
