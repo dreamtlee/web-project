54845582986dbf18ea5f4bf669f0973c1153bd36
dreamtlee <deniil-bog@mail.ru>
2026-05-12 20:58:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
+
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fitbot-secret-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fitbot-v2-ultra-secret-key-999'
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'fitbot.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    YANDEX_GEO_API_KEY = '8013b162-6b42-4997-9691-77b7074026e0'
-    YANDEX_WEATHER_API_KEY = 'ВСТАВЬ КЛЮЧ ПОГОДЫ!'
-    
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
+    YANDEX_GEO_API_KEY = '8013b162-6b42-4997-9691-77b7074026e0'
+    YANDEX_WEATHER_API_KEY = 'fa0f11a5-fd86-48c2-b07f-ef6e45a933a8'
